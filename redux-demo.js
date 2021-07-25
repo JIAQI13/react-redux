ba764ba04d694cee9e0622b446f22fca723d6bc9
Jiaqi Liu <jiaqiliu@Jiaqis-Mac-mini.local>
2021-07-24 19:46:28 -0700

    add decrement

--- redux-demo.js
+++ redux-demo.js
@@ -1,16 +1,25 @@
 const redux = require("redux");
 
 //reducer
-const countReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+const countReducer = (state = { counter: 1 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
 };
 
 //store
 const store = redux.createStore(countReducer);
 
-console.log(store.getState());
+console.log("log1", store.getState());
 //sub
 const counterSubscriber = () => {
   const lastestState = store.getState();
@@ -18,3 +27,6 @@ const counterSubscriber = () => {
 };
 
 store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
